1fdb6165680bd5d2d6d1aa79bab647ed6c1ad1db
Dev-Yoko <gadgetlabreviews@gmail.com>
2024-03-24 19:21:49 +0530

    BotZone

--- src/components/widgets/Stats.tsx
+++ src/components/widgets/Stats.tsx
@@ -3,10 +3,20 @@ import { component$ } from "@builder.io/qwik";
 export default component$(() => {
   return (
     <div class="px-4 py-8 md:py-16 sm:px-6 mx-auto md:px-24 lg:px-8 lg:py-20 max-w-7xl">
-      <div class="grid grid-cols-2 row-gap-8 md:grid-cols-4">
-        <div class="text-center md:border-r dark:md:border-slate-500 mb-10 md:mb-0">
+      <div class="grid grid-cols-3 gap-8">
+        <div class="text-left">
           <div class="text-4xl font-bold lg:text-5xl xl:text-6xl text-[#039de1] font-heading">
-            Support Our BotZone, Subscribe our Youtube Channel & Stay Updated with BotZone
+            Support Our BotZone
+          </div>
+        </div>
+        <div class="text-center">
+          <div class="text-4xl font-bold lg:text-5xl xl:text-6xl text-[#ff6347] font-heading">
+            Subscribe our Youtube Channel
+          </div>
+        </div>
+        <div class="text-right">
+          <div class="text-4xl font-bold lg:text-5xl xl:text-6xl text-[#7cfc00] font-heading">
+            Stay Updated with BotZone
           </div>
         </div>
       </div>
